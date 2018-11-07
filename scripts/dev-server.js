a121d11df0759fda79b5928669dd9f1a0e194585
毛庭峰 <hzmaotingfeng@corp.netease.com>
2018-11-07 14:12:20 +0800

    site init

---
+++ scripts/dev-server.js
@@ -0,0 +1,52 @@
+// webpack-dev-server --open --inline --hot --port 8022 --config scripts/webpack.dev.config.js
+
+const path = require( 'path' )
+const HtmlWebpackPlugin = require( 'html-webpack-plugin' )
+const webpack = require( 'webpack' )
+const webpackBaseConfig = require( './webpack.base.config.js' )
+const merge = require('webpack-merge')
+const webpackDevServer = require( 'webpack-dev-server' )
+const { site } = require( './project-path' )
+
+const siteResolve = ( ...args ) => path.resolve( site , ...args )
+const port = 8022 ,
+    host = 'localhost'
+const config = merge( webpackBaseConfig , {
+    devtool: 'eval-source-map',
+    mode: 'development',
+    entry: {
+        main: siteResolve( 'index.js' ) ,
+        vendors: [ 'vue', 'vue-router' ]
+    },
+    output: {
+        path: siteResolve( '../dist' ) ,
+        publicPath: '' ,
+        filename: '[name].js',
+        chunkFilename: '[name].chunk.js'
+    } ,
+    plugins: [
+        new HtmlWebpackPlugin( {
+            inject: true,
+            filename: siteResolve( '../dist' , 'index.html' ) ,
+            template: siteResolve( 'index.html' ) ,
+        } )
+    ]
+} )
+
+const devOptions = {
+    contentBase: [ site ] ,
+    proxy: {
+
+    } ,
+    hot: true ,
+    compress: true ,
+}
+
+const webpackCompiler = webpack( config )
+
+const server = new webpackDevServer( webpackCompiler , devOptions )
+
+server.listen( port , host , function() {
+    console.log( 'done' )
+} )
+
---
+++ scripts/project-path.js
@@ -0,0 +1,13 @@
+const path = require( 'path' )
+
+const root = process.cwd()
+
+function resolvePath( dir ) {
+    return path.resolve( root , dir )
+}
+
+
+module.exports = {
+    site: resolvePath( './site' ) ,
+    
+}
