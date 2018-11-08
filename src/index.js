6429d506cf682df15ae4a2fcd5020c1b32734845
毛庭峰 <hzmaotingfeng@corp.netease.com>
2018-11-08 15:53:08 +0800

    精简目录结构

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
-import Button from './components/button';
+import Button from './components/button'
 
 const components = {
     Button ,
 }
 
-// @TODO 命名规范？
-// 统一默认前缀
 const install = function( Vue , opts = {} ) {
     Object.keys( components ).forEach( key => {
         Vue.component( key , components[ key ] )
     } )
-    // Confirm Toast加到全局 @TODO
-    Vue.toast = Vue.prototype.$toast = _toastInstance;
 }
 
 export {
-    Button,
+    Button ,
 }
 
 export default {
