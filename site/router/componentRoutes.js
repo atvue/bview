a428934f09360c82396e7e8e900d904120cdce50
lcoder <mtingfeng@gmail.com>
2018-12-03 14:04:02 +0800

    抽出组件列表的子路由部分

---
+++ site/router/componentRoutes.js
@@ -0,0 +1,32 @@
+
+export const componentRoutes = [{
+        path: 'button',
+        meta: {
+            name: '按钮'
+        }
+    } ,
+    {
+        path: 'select',
+        meta: {
+            name: '下拉选框'
+        }
+    } ,
+    {
+        path: 'Input',
+        meta: {
+            name: '单行输入框'
+        }
+    } ,
+    {
+        path: 'Switches',
+        meta: {
+            name: '开关'
+        }
+    } ,
+    {
+        path: 'Textarea',
+        meta: {
+            name: '多行输入框'
+        }
+    }
+]
