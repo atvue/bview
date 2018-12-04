4a6738c28c1041bff724648dec28df163b48cbbe
lcoder <mtingfeng@gmail.com>
2018-12-04 14:53:49 +0800

    移入移除dropdown设置缓冲时间

--- src/components/Dropdown/Dropdown.vue
+++ src/components/Dropdown/Dropdown.vue
@@ -1,22 +1,31 @@
-<atemplate>
-    <span :class="prefixCls">
+<template>
+    <span 
+        :class="prefixCls"
+        @mouseenter="_mouseEnter"
+        @mouseleave="_mouseLeave"
+    >
         <slot></slot>
         <div 
+            ref="overlay"
             v-if="visible"
-            :class="clsOverlay"
             v-dom-portal
+            :class="clsOverlay"
         >
             <slot name="overlay" />
         </div>
     </span>
-</atemplate>
+</template>
 
 
 <script>
 import domPortal from '../../directives/dom-portal'
 import warnInit from '../../utils/warn'
+import makeCancelable from '../../utils/makeCancelable'
+import { timeout } from '../../utils/timer'
+import noop from '../../utils/noop'
 const name = 'dropdown'
 const warn = warnInit( name )
+const lazy = 200
 const placement = {
     bottomLeft: 'bottomLeft'
 }
@@ -32,7 +41,9 @@ export default {
     } ,
     data(){
         return {
-            visible: true ,
+            visible: false ,
+            cancelEnter: noop ,
+            cancelLeave: noop ,
         }
     } ,
     computed: {
@@ -44,46 +55,39 @@ export default {
         }
     } ,
     methods: {
-        bindEvent(){
-            let { $slots: { default: defaultNodes } } = this ,
-                validEl = defaultNodes.find( () => {} )
-            // console.log( defaultNodes )
-        } ,
-        mouseEnter(){
-            this.visible = true
-        } ,
-        mouseLeave(){
-            this.visible = false
-        }
-    } ,
-    render( h ){
-        let { $slots: { default: defaultNodes , overlay } , prefixCls
-             , clsOverlay , visible } = this ,
-            hasDefaultSlots = defaultNodes !== undefined ,
-            hasOverlay = overlay !== undefined
-        if ( !hasDefaultSlots ) {
-            throw new Error( `${name}必须包裹一个元素`  )
-        }
-        if ( !hasOverlay || visible === false ) {
-            overlay = []
-        }
-        let overlayEl = h( 'div' , {
-                directives: [ domPortal ] ,
-                'class': [ clsOverlay ] ,
-            } , [ ...overlay ] )
-        return h( 'span' ,
-            {
-                "class": [ prefixCls ] ,
-                on: {
-                   mouseenter: this.mouseEnter ,
-                   mouseleave: this.mouseLeave ,
+        async _mouseEnter( event ){
+            let { cancelLeave } = this ,
+                { promise , cancel: cancelEnter } = makeCancelable( timeout( lazy ) )
+            cancelLeave()
+            this.cancelEnter = cancelEnter
+            try {
+                let { target } = event
+                await promise
+                this.visible = true
+                // 计算位置
+                await this.$nextTick()
+                let { $refs: { overlay } } = this
+            } catch( e ) {
+                if ( !e.isCanceled ) {
+                    console.warn( e ) 
                 }
-            } ,
-            [
-                ...defaultNodes ,
-                overlayEl ,
-            ]
-        )
+            }
+        } ,
+        async _mouseLeave(){
+            let { cancelEnter } = this
+            cancelEnter()
+            let { promise , cancel: cancelLeave } = makeCancelable( timeout( lazy ) )
+            this.cancelLeave = cancelLeave
+            try {
+                await promise
+                this.visible = false
+            } catch( e ) {
+                if ( !e.isCanceled ) {
+                    console.warn( e ) 
+                }
+            }
+        } ,
+
     }
 }
 </script>
--- src/directives/dom-portal.js
+++ src/directives/dom-portal.js
@@ -11,33 +11,26 @@ const homes = new Map()
 const domPortal = {
     name: 'dom-portal' ,
     inserted( el , { value } , vnode) {
-        const { parentNode } = el
-        const home = document.createComment('')
+        const inited = homes.has( el )
         let hasMovedOut = false
 
-        if (value !== false) {
-            parentNode.replaceChild(home, el) // moving out, el is no longer in the document
-            getTarget(value).appendChild(el) // moving into new place
+        if ( value !== false ) {
+            getTarget( value ).appendChild( el ) // moving into new place
             hasMovedOut = true
         }
-
-        if ( !homes.has( el ) ) {
-            homes.set(el, { parentNode, home, hasMovedOut }) // remember where home is or should be
+        if ( !inited ) {
+            homes.set( el , { 
+                hasMovedOut
+            } )
         }
     } ,
     componentUpdated( el , { value } ) {  // need to make sure children are done updating (vs. `update`)
-        const { parentNode, home, hasMovedOut } = homes.get( el ) // recall where home is
+        const { hasMovedOut } = homes.get( el )
 
         if ( !hasMovedOut && value ) {
-            // remove from document and leave placeholder
-            parentNode.replaceChild(home, el)
-            // append to target
             getTarget(value).appendChild(el)
-
-            homes.set(el, Object.assign({}, homes.get(el), { hasMovedOut: true }))
+            homes.set( el , Object.assign({}, homes.get(el), { hasMovedOut: true }))
         } else if ( hasMovedOut && value === false ) {
-            // previously moved, coming back home
-            parentNode.replaceChild(el, home)
             homes.set(el, Object.assign({}, homes.get(el), { hasMovedOut: false }))
         } else if (value) {
             // already moved, going somewhere else
@@ -45,7 +38,7 @@ const domPortal = {
         }
     } ,
     unbind( el ) {
-        homes.delete(el)
+        homes.delete( el )
     }
 }
   
