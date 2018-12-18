61b7d98c7789ec920bc7f98053a4bd3db8bcf1f2
lcoder <mtingfeng@gmail.com>
2018-12-18 20:04:21 +0800

    引入dom-align

---
+++ src/components/Dropdown/dom-align/index.js
@@ -0,0 +1,10 @@
+/**
+ * source code: https://github.com/yiminghe/dom-align
+ */
+
+import alignElement from './align/alignElement';
+import alignPoint from './align/alignPoint';
+
+export { alignElement, alignPoint };
+
+export default alignElement;
