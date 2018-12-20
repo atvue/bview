0323219b72c41d36d1cad2a3bad3001038695b54
lcoder <mtingfeng@gmail.com>
2018-12-20 15:06:29 +0800

    同步检测文件是否存在，新增忽略不解析指定vue文件的.sfcdocignore配置的功能

--- scripts/fs-helper/existsFile.js
+++ scripts/fs-helper/existsFile.js
@@ -1,8 +1,6 @@
 const fs = require('fs')
 
-
-
-module.exports = function( filename ) {
+const existsFile = function( filename ) {
     return new Promise( ( complete , fail ) => {
         fs.access( filename , fs.R_OK | fs.W_OK , err => {
             if ( err ) {
@@ -13,3 +11,16 @@ module.exports = function( filename ) {
         } )
     } )
 }
+
+const existsFileSync = function( filename ) {
+    try {
+        fs.accessSync( filename , fs.constants.R_OK | fs.constants.W_OK )
+        return true
+    } catch (err) {
+        return false
+    }
+}
+
+exports.existsFile = existsFile
+
+exports.existsFileSync = existsFileSync
