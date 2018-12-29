659fdf4066f6a2ca46a3c8d1c66b95207660df2c
lcoder <mtingfeng@gmail.com>
2018-12-29 16:23:36 +0800

    升级vue-router 支持RouterLink的写法:https://github.com/vuejs/vue-router/releases/tag/v3.0.2

--- site/app.vue
+++ site/app.vue
@@ -4,11 +4,14 @@
         <div style="display:flex">
             <!-- 随便生成的菜单 待替换 -->
             <div style="width: 160px;margin-top:20px;flex:0 0 160px;">
-                <div v-for="(r,i) in routerData" :key="getMetaName(r,i)">
-                    <router-link :to="r.path">{{getMetaName(r,i)}}</router-link>
+                <div 
+                    v-for="(r,i) in routerData" 
+                    :key="getMetaName(r,i)"
+                >
+                    <RouterLink :to="r.path">{{getMetaName(r,i)}}</RouterLink>
                     <div v-show="r.children&&r.children.length>0">
                         <div v-for="(c,j) in r.children" :key="getMetaName(c,j)" style="padding-left:10px;">
-                            <router-link :to="r.path+'/'+c.path">{{getMetaName(c,j)}}</router-link>
+                            <RouterLink :to="r.path+'/'+c.path">{{getMetaName(c,j)}}</RouterLink>
                         </div>
                     </div>
                 </div>
