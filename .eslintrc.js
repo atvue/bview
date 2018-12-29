bf6164f1892380909c439d26d2fed7fca2fd34b3
lcoder <mtingfeng@gmail.com>
2018-12-29 16:41:28 +0800

    添加vue/no-unused-vars规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,12 @@
 
 module.exports = {
     extends: [
-        // add more generic rulesets here, such as:
-        // 'eslint:recommended',
-        'plugin:vue/recommended'
-    ],
+        // 'eslint:recommended' ,
+        'plugin:vue/recommended' ,
+    ] ,
     rules: {
         "indent": [ "warn" , 4 ] ,
         "vue/html-indent": [ "warn" , 4 ] ,
-        // 'vue/no-unused-vars': 'warn' ,
+        "vue/no-unused-vars": 'warn' ,
     }
 }
