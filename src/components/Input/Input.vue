54471d7ece2817a5cba013b0c2523c713ed358be
icyfanfan <kisscat1990114@163.com>
2019-01-02 18:56:05 +0800

    merge

--- src/components/Input/Input.vue
+++ src/components/Input/Input.vue
@@ -1,6 +1,7 @@
 <template >
     <div :class="prefixCls+'-wrapper'" :style="{'width':width}">
         <span :class="[prefixCls + '-icon-prepend']" v-if="$slots.prepend||prependIcon">
+            <!-- 输入框内部：左侧（前）预留位 -->
             <slot name="prepend"></slot>
             <i :class="['iconfont',prependIcon]" v-if="prependIcon"></i>
         </span>
@@ -8,15 +9,17 @@
         <i v-else-if="search" :class="['iconfont icon-sousuokuang-sousuo',prefixCls+'-icon',prefixCls+'-icon-search']" @click="_handleSearch"></i>
         <input ref="input" :placeholder="placeholder" :class="inputClasses" :autocomplete="autocomplete" :maxlength="maxlength" :value="currentValue" @input="_handleInput" :autofocus="autofocus" @focus="_handleFocus" @blur="_handleBlur" :readonly="readonly" :disabled="disabled" :type="type" @keyup.enter="_handleEnter">
         <span :class="[prefixCls + '-icon-append']" v-if="$slots.append||appendIcon">
+            <!-- 输入框：右侧（后）预留位 -->
             <slot name="append"></slot>
             <i :class="['iconfont',appendIcon]" v-if="appendIcon"></i>
         </span>
+        <!-- 默认插槽，输入框外部后方 -->
         <slot></slot>
     </div>
 
 </template>
 <script>
-const prefixCls = 'bui-input';
+const prefixCls = 'bview-input';
 export default {
     name: 'Input',
     props: {
@@ -38,7 +41,7 @@ export default {
         //@doc是否启用自动完成功能，为on或者off
         autocomplete: {
             type: String,
-            default: 'off'
+            default: 'on'
         },
         //@doc是否自动聚焦
         autofocus: {
@@ -115,15 +118,7 @@ export default {
             let value = event.target.value;
             //@doc 输入框改变时触发
             this.$emit('input', value);
-            this.setCurrentValue(value);
-        },
-        //@doc手动聚焦
-        focus() {
-            this.$refs.input.focus();
-        },
-        //@doc手动失焦
-        blur() {
-            this.$refs.input.blur();
+            this._setCurrentValue(value);
         },
         _handleFocus(event) {
             //@doc聚焦时触发
@@ -136,15 +131,23 @@ export default {
         _handleClear() {
             const e = { target: { value: '' } };
             this.$emit('input', '');
-            this.setCurrentValue('');
+            this._setCurrentValue('');
         },
         _handleSearch(event) {
             this.$emit('search', event, this.currentValue);
-        }
+        },
+        //@doc手动聚焦
+        focus() {
+            this.$refs.input.focus();
+        },
+        //@doc手动失焦
+        blur() {
+            this.$refs.input.blur();
+        },
     },
     watch: {
         value(val) {
-            this.setCurrentValue(val);
+            this._setCurrentValue(val);
         }
     }
 };
--- src/components/Input/demo/basic1.md
+++ src/components/Input/demo/basic1.md
@@ -3,7 +3,7 @@
   <section>
     <div class="example">
       <span>test1:</span>
-      <Input placeholder="请输入" v-model="value1" @blur="blur" @change="change" width="300px" />
+      <Input placeholder="请输入" v-model="value1" @blur="blur" @change="change" width="300px" clearable />
     </div>
     <div class="example">
       <span>test2:</span>
