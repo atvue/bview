6175192b65f4d98ed4481804a0ae8152d371c4a6
icyfanfan <kisscat1990114@163.com>
2019-01-07 15:29:18 +0800

    缓存

--- scripts/sfc-doc/parser/parseScript.js
+++ scripts/sfc-doc/parser/parseScript.js
@@ -85,127 +85,131 @@ module.exports = astJs => {
     propsRes = [];
     apiMethods = [];
     traverse(astJs, {
-        Property(path) {
-            // props
-            if (path.node.key.name === 'props') {
-                let propsPath = path.get('value');
-                // debugger;
-                // let
-                propsPath.traverse({
-                    Property(path) {
-                        if (path.parentPath !== propsPath) {
-                            // 仅查找同级节点，parent都是props
-                            return;
-                        }
-
-                        let prop = {
-                            name: '',
-                            type: undefined,
-                            describe: '',
-                            required: false,
-                            default: undefined,
-                            validator: undefined
-                        };
-                        prop.name = path.node.key.name;
-                        // 获取注释
-                        // @NOTE 暂定为获取距离最近的一行
-                        prop.describe = getComment(path.node);
-                        // debugger;
-                        // 判断prop的值
-                        let { value } = path.node;
-                        // debugger;
-                        // xxx: {type: xxx ... } 形式
-                        if (bt.isObjectExpression(value)) {
-                            path.traverse({
-                                Property(path) {
-                                    // debugger;
-                                    let { name } = path.node.key,
-                                        { value } = path.node;
-                                    switch (name) {
-                                        case 'type':
-                                            // debugger;
-                                            prop.type = getType(value);
-                                            break;
-                                        case 'required':
-                                            // debugger;
-                                            prop.required = value.value;
-                                            break;
-                                        case 'default':
-                                            prop[name] = getDefault(value);
-                                            break;
-                                        case 'validator':
-                                            // 仅获取注释
-                                            prop[name] = getComment(path.node);
-                                            break;
-                                        default:
-                                            break;
+        ExportDefaultDeclaration (exportDefaultPath){
+            exportDefaultPath.traverse( {
+                Property(path) {
+                // props
+                if (path.node.key.name === 'props') {
+                    let propsPath = path.get('value');
+                    // debugger;
+                    // let
+                    propsPath.traverse({
+                        Property(path) {
+                            if (path.parentPath !== propsPath) {
+                                // 仅查找同级节点，parent都是props
+                                return;
+                            }
+    
+                            let prop = {
+                                name: '',
+                                type: undefined,
+                                describe: '',
+                                required: false,
+                                default: undefined,
+                                validator: undefined
+                            };
+                            prop.name = path.node.key.name;
+                            // 获取注释
+                            // @NOTE 暂定为获取距离最近的一行
+                            prop.describe = getComment(path.node);
+                            // debugger;
+                            // 判断prop的值
+                            let { value } = path.node;
+                            // debugger;
+                            // xxx: {type: xxx ... } 形式
+                            if (bt.isObjectExpression(value)) {
+                                path.traverse({
+                                    Property(path) {
+                                        // debugger;
+                                        let { name } = path.node.key,
+                                            { value } = path.node;
+                                        switch (name) {
+                                            case 'type':
+                                                // debugger;
+                                                prop.type = getType(value);
+                                                break;
+                                            case 'required':
+                                                // debugger;
+                                                prop.required = value.value;
+                                                break;
+                                            case 'default':
+                                                prop[name] = getDefault(value);
+                                                break;
+                                            case 'validator':
+                                                // 仅获取注释
+                                                prop[name] = getComment(path.node);
+                                                break;
+                                            default:
+                                                break;
+                                        }
                                     }
-                                }
-                            });
-                        } else {
-                            // xxx: xxx 或 xxx: [xx,xxx] 形式
-                            // 仅定义了prop的类型
-                            prop.type = getType(value);
+                                });
+                            } else {
+                                // xxx: xxx 或 xxx: [xx,xxx] 形式
+                                // 仅定义了prop的类型
+                                prop.type = getType(value);
+                            }
+    
+                            // 将validator合并到describe中
+                            if (prop.validator !== undefined) {
+                                prop.describe = `${prop.describe} ${
+                                    prop.validator
+                                }`;
+                            }
+                            propsRes.push(prop);
                         }
-
-                        // 将validator合并到describe中
-                        if (prop.validator !== undefined) {
-                            prop.describe = `${prop.describe} ${
-                                prop.validator
-                            }`;
-                        }
-                        propsRes.push(prop);
-                    }
-                });
-            }
-            // methods
-            // @note 改成用jsdoc解析注释结构 生成描述、参数和返回值说明
-            if (path.node.key.name === 'methods') {
-                let { properties } = path.node.value;
-                apiMethods = properties
-                    .filter(e => {
-                        return /^[^_]/.test(e.key.name);
-                    })
-                    .map(e => {
-                        return {
-                            name: e.key.name,
-                            describe: getComment(e)
-                        };
                     });
+                }
+                // methods
+                // @note 改成用jsdoc解析注释结构 生成描述、参数和返回值说明
+                if (path.node.key.name === 'methods') {
+                    let { properties } = path.node.value;
+                    apiMethods = properties
+                        .filter(e => {
+                            return /^[^_]/.test(e.key.name);
+                        })
+                        .map(e => {
+                            return {
+                                name: e.key.name,
+                                describe: getComment(e)
+                            };
+                        });
+                    // debugger;
+                }
+            },
+            // 从expressionstatement中过滤出$emit
+            CallExpression(path) {
                 // debugger;
-            }
-        },
-        // 从expressionstatement中过滤出$emit
-        CallExpression(path) {
-            // debugger;
-            if (
-                bt.isMemberExpression(path.node.callee) &&
-                path.node.callee.property.name === '$emit'
-            ) {
-                let { arguments } = path.node;
-                // 第一个是事件名称，第二个是事件传参
-                let [emitName, emitArg] = arguments;
-                // @NOTE 这里不能判断去重 traverse不是一个同步过程
-                // 检查emitEvents中是否存在 emitKey
-                // let exist =
-                //     emitEvents.indexOf(e => {
-                //         // console.log(e.name, emitName.value);
-                //         debugger;
-                //         return e.name === emitName.value;
-                //     }) !== -1;
-                // debugger;
-
-                // if (!exist) {
-                // 参数说明和事件说明都解析注释
-                let emit = {
-                    name: emitName.value,
-                    args: generate(emitArg).code, // args暂时没有用
-                    describe: getComment(path.parent)
-                };
-                emitEvents.push(emit);
-                // }
-            }
+                if (
+                    bt.isMemberExpression(path.node.callee) &&
+                    path.node.callee.property.name === '$emit'
+                ) {
+                    let { arguments } = path.node;
+                    // 第一个是事件名称，第二个是事件传参
+                    let [emitName, emitArg] = arguments;
+                    // @NOTE 这里不能判断去重 traverse不是一个同步过程
+                    // 检查emitEvents中是否存在 emitKey
+                    // let exist =
+                    //     emitEvents.indexOf(e => {
+                    //         // console.log(e.name, emitName.value);
+                    //         debugger;
+                    //         return e.name === emitName.value;
+                    //     }) !== -1;
+                    // debugger;
+    
+                    // if (!exist) {
+                    // 参数说明和事件说明都解析注释
+                    let emit = {
+                        name: emitName.value,
+                        args: generate(emitArg).code, // args暂时没有用
+                        describe: getComment(path.parent)
+                    };
+                    emitEvents.push(emit);
+                    // }
+                }
+            }})
         }
+       
     });
     // emitEvents去重
     let deEmitEvents = [];
