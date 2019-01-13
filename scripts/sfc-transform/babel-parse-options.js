405527ece63ae997bbe20127d94f6c37c4873c47
lcoder <mtingfeng@gmail.com>
2019-01-13 15:44:47 +0800

    fix babel This experimental syntax requires enabling the parser plugin: 'objectRestSpread'

---
+++ scripts/sfc-transform/babel-parse-options.js
@@ -0,0 +1,9 @@
+
+
+
+exports.babelParseOptions = {
+    plugins: [
+        'transform-object-rest-spread' ,
+        [ '@babel/plugin-transform-modules-commonjs' , { strictMode: true } ] ,
+    ]
+}
