4d163479c30a60aac9d723f21f94b82e6073c8f8
lcoder <mtingfeng@gmail.com>
2019-01-22 15:07:02 +0800

    允许v-html

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
         "indent": ["warn", 4],
         "vue/html-indent": ["warn", 4],
         "vue/no-unused-vars": 'warn',
+        "vue/no-v-html": 0 ,
     },
     env: {
         "browser": true,
