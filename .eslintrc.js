266800c65bb6b909c7ba2b9e32c2cd9c1ecae18f
lcoder <mtingfeng@gmail.com>
2019-01-22 16:37:59 +0800

    添加jest环境

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,18 @@
 module.exports = {
+    env: {
+        "browser": true ,
+        "commonjs": true ,
+        "node": true ,
+        "jest": true ,
+    } ,
     extends: [
         'eslint:recommended',
         'plugin:vue/recommended',
-    ],
+    ] ,
     rules: {
         "indent": ["warn", 4],
         "vue/html-indent": ["warn", 4],
         "vue/no-unused-vars": "warn" ,
         "vue/no-v-html": "off" ,
-    },
-    env: {
-        "browser": true ,
-        "commonjs": true ,
-        "node": true ,
-    }
+    } ,
 }
