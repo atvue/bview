ae5be1414e3d5bc41c8bc240790040377717edac
icyfanfan <kisscat1990114@163.com>
2019-01-23 17:31:48 +0800

    增加ndp构建用shell脚本(暂时)

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+
+export PATH=/home/ndp-soft/node-v8.11.3-linux-x64/bin:$PATH
+
+/home/ndp-soft/node-v8.11.3-linux-x64/node_modules/yarn/bin/yarn
+
+/home/ndp-soft/node-v8.11.3-linux-x64/node_modules/yarn/bin/yarn site
+
+
+
