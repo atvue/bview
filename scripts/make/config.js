6b9c6f348dc51efcadc1694159a8a9e261e58bac
lcoder <mtingfeng@gmail.com>
2019-01-23 19:41:30 +0800

    模板优化

--- scripts/make/config.js
+++ scripts/make/config.js
@@ -1,4 +1,4 @@
-const { firstUpperCase } = require( './util' )
+const { firstUpperCase: upCase } = require( './util' )
 
 //template文件结构
 function createConfig(name) {
@@ -7,17 +7,22 @@ function createConfig(name) {
         child: [
             {
                 dir: '__tests__',
-                child: [{
-                    file: `${name}.test.js`,
-                    temp: 'testJs'
-                }]
-            },
+                child: [
+                    {
+                        file: `${name}.test.js`,
+                        temp: 'testJs'
+                    }
+                ]
+            } ,
             {
                 dir: 'demo',
-                child: [{
-                    file: 'basic.md',
-                }]
-            },
+                child: [
+                    {
+                        file: 'basic.md' ,
+                        temp: 'demoTemp'
+                    }
+                ]
+            } ,
             {
                 dir: 'style',
                 child: [
@@ -40,65 +45,104 @@ function createConfig(name) {
 
             },
             {
-                file: 'README.st'
+                file: 'README.st' ,
+                temp: 'readmeTemp'
             }
         ],
     }
 }
 
-let vueTemp = name => `<template>
-	<div></div>
+const readmeTemp = name => {
+    return (
+        `
+---
+title: ${ upCase( name ) }
+---
+### 组件名称
+
+组件介绍...
+
+### 场景说明{id="changjin"}
+ 
+组件使用场景...
+`.trim()
+    ) }
+
+const vueTemp = name => (
+    `
+<template>
+    <div />
 </template>
 
 <script>
-	export default {
-		name: '${name}',
-		props: {
+export default {
+    name: '${ upCase( name ) }' ,
+    props: {
+    } ,
+    data () {
+        return {
+        }
+    } ,
+    methods: {
+    } ,
+}
+</script>
+`.trim()
+)
 
-        } ,
-		data () {
-			return {
+const styleIndex = () => `import './index.less'`
 
-            }
-		},
-		methods: {
-
-        } ,
-	}
-</script>`
-
-let styleIndex = () => `import './index.less'`
-
-let indexJs = name => {
-    let up = firstUpperCase(name)
+const indexJs = name => {
+    let up = upCase(name)
 
     return `import ${up} from './${name}'
 
 export default ${up}`
 }
 
-let testJs = name => {
-    name = firstUpperCase( name )
+const testJs = name => {
+    name = upCase( name )
 
-    return `import ${name} from '../index.js'
+    return (
+        `
+import ${name} from '../index.js'
+import { mount } from '@vue/test-utils'
 
 describe( '${name}' , () => {
-    import ${name} from '../index.js'
-    import { mount } from '@vue/test-utils'
 
     test( '创建${name}成功' , () => {
         const wrapper = mount( ${name} )
         expect( wrapper.isVueInstance() ).toBeTruthy()
     } )
 })
+`.trim()
+    )
+}
+
+const demoTemp = name => {
+    return (
+        `
+\`\`\`vue
+<template>
+    <div>这里编写Tag2的demo</div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+\`\`\`
 `
+    )
 }
 
 exports.createConfig = createConfig
 
 exports.tp = {
-    vueTemp,
-    styleIndex,
-    indexJs,
-    testJs
+    vueTemp ,
+    styleIndex ,
+    indexJs ,
+    testJs ,
+    readmeTemp ,
+    demoTemp ,
 }
