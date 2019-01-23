ab4ba0f9b4aaad5cb11ad179be2fa4ea6dc9e128
lcoder <mtingfeng@gmail.com>
2019-01-23 20:07:32 +0800

    兼容某些原因被忽略加入cpGroups文件后，通过网站不能访问到的情况

--- site/router/cpGroups.js
+++ site/router/cpGroups.js
@@ -1,6 +1,5 @@
-/* eslint-disable */
-
-const group = [{
+const group = [
+    {
         categoryName: '基础组件',
         components: ['button', 'icon']
     },
@@ -15,7 +14,7 @@ const group = [{
             'cascader',
             'select',
             'switches',
-            'search'
+            'search',
         ]
     },
     {
@@ -28,24 +27,51 @@ const group = [{
     }
 ];
 
+
 export function resetGroup(routerData, rootRoute) {
-    return group.map(item => {
+    let allComponents = routerData.map( ( { path } ) => path ) ,
+        defined = [] ,
+        result = group.map(item => {
+            let obj = {
+                meta: {
+                    name: item.categoryName
+                } ,
+                children: [] ,
+                path: '/components' ,
+                component: rootRoute
+            }
+            item.components.forEach( com => {
+                let element = routerData.find( route => route.path === com ) ,
+                    has = element !== undefined
+                if ( has ) {
+                    defined.push( com )
+                    obj.children.push( element )
+                }
+            } )
+            return obj
+        } ) ,
+        a = new Set( allComponents ) ,
+        b = new Set( defined ) ,
+        diffSet = new Set( [ ...a ].filter( x => !b.has( x ) ) ) ,
+        diffArr = Array.from( diffSet ) ,
+        hasDiff = diffArr.length > 0
+    if ( hasDiff ) {
         let obj = {
             meta: {
-                name: item.categoryName
-            },
-            children: [],
-            path: '/components',
+                name: '未定义'
+            } ,
+            children: [] ,
+            path: '/components' ,
             component: rootRoute
-        };
-        item.components.forEach(com => {
-            let element = routerData.find(route => {
-                return route.path === com;
-            });
-            if (element) {
-                obj.children.push(element);
+        }
+        diffArr.forEach( com => {
+            let element = routerData.find( route => route.path === com ) ,
+                has = element !== undefined
+            if ( has ) {
+                obj.children.push( element )
             }
-        });
-        return obj;
-    });
+        } )
+        result.push( obj )
+    }
+    return result
 }
